2e0878c852bec227908f40b24d24202f565c1727
leezye <aetwaroo@accudocsolutions.com>
2017-06-09 19:30:11 -0400

    update

--- brunch-config.js
+++ brunch-config.js
@@ -1,16 +1,18 @@
-// See http://brunch.io for documentation.
 exports.files = {
-  javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'}
+	javascripts: {
+		joinTo: 'app.js'
+	},
+	stylesheets: {
+		joinTo: 'app.css'
+	}
 };
-
 exports.plugins = {
-    babel: {
-        presets: ['es2015']
-    },
-    sass: {
-options: {
-includePaths: ['node_modules/bootstrap/scss']
-}
-}
+	babel: {
+		presets: ['es2015']
+	},
+	sass: {
+		options: {
+			includePaths: ['node_modules/bootstrap/scss']
+		}
+	}
 }
