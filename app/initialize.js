4fd327304b51de52c94411d0b98ac8d7cf80839d
leezye <aetwaroo@accudocsolutions.com>
2017-06-10 02:40:04 -0400

    update

--- app/initialize.js
+++ app/initialize.js
@@ -15,29 +15,36 @@ import routes from './routes'
 
 //create Vue instance with basic routing 
 document.addEventListener('DOMContentLoaded', function() {
-	// load vue 
-	var vue = require('vue');
+    // load vue 
+    var vue = require('vue');
+    
+    // load scrollreveal 
     var ScrollReveal = require('scrollreveal');
-    window.sr = ScrollReveal({ reset: true });
+    window.sr = ScrollReveal({
+        reset: true
+    });
     sr.reveal('.fade-reveal');
 
-	const app = new Vue({
-		el: '#app',
-		data: {
-			ViewComponent: {
+    // create vue instance
+    const app = new Vue({
+        el: '#app',
+        data: {
+            ViewComponent: {
                 // root container
-				render: h => h('div', 'loading...')
-			}
-		},
-		render(h) {
-			return h(this.ViewComponent)
-		}
-	})
-	Object.keys(routes).forEach(route => {
-		const Component = require('./pages/' + routes[route] + '.vue')
-		page(route, () => app.ViewComponent = Component)
-	})
-	page('*', () => app.ViewComponent = require('./pages/404.vue'))
-	page()
-      
+                render: h => h('div', 'loading...')
+            }
+        },
+        render(h) {
+            return h(this.ViewComponent)
+        }
+    })
+    
+    //routing
+    Object.keys(routes).forEach(route => {
+        const Component = require('./pages/' + routes[route] + '.vue')
+        page(route, () => app.ViewComponent = Component)
+    })
+    page('*', () => app.ViewComponent = require('./pages/404.vue'))
+    page()
+
 });
